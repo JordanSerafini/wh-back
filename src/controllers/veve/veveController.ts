4d1439d1fb557caef8163543160409622290ab19
Jordan S <jordanserafini.74@gmail.com>
2024-04-01 11:39:07 +0200

    :sparkles: setup

--- src/controllers/veve/veveController.ts
+++ src/controllers/veve/veveController.ts
@@ -74,3 +74,5 @@ const veveController = {
     }
 
 };
+
+export default veveController;
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -2,6 +2,7 @@ import express from "express";
 
 import utilisateurController from "../controllers/controller";
 import poiController from "../controllers/poiController";
+import veveController from "../controllers/veve/veveController";
 
 const router = express.Router();
 
@@ -16,5 +17,15 @@ router.patch("/poi/:id", poiController.update);
 router.delete("/poi/:id", poiController.delete);
 router.get("/poi/nom/:nom", poiController.getByName);
 router.get("/poi/owner/:owner", poiController.getByOwner);
+
+// Route veve
+router.get("/veve", veveController.getAll);
+router.get("/veve/:id", veveController.getOne);
+router.post("/veve", veveController.create);
+router.patch("/veve/:id", veveController.update);
+router.delete("/veve/:id", veveController.delete);
+router.get("/veve/nom/:nom", veveController.getByName);
+router.get("/veve/owner/:owner", veveController.getByOwner);
+
 
   export default router
